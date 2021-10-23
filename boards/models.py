ec996f66fbd00431eae7fb9f6252972aa6c34e93
dongheon yu <ptw880@gmail.com>
2021-10-24 01:15:45 +0900

    board/post method views.py, tests.py

--- boards/models.py
+++ boards/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from django.db.models.fields.related import ForeignKey
-        
-class Board(models.Model):
-    writer    = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    title     = models.CharField(max_length=45)
-    content   = models.CharField(max_length=500)
-    password  = models.CharField(max_length=5000, default=None)
-    hits      = models.PositiveIntegerField(default=0)
-    tag       = models.ForeignKey('Tag', on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return '[{}] {}'.format(self.id, self.title)
-    
-    class Meta:
-        db_table = 'boards'
-        
+
 class Category(models.Model):
     name = models.CharField(max_length=45)
     
@@ -26,10 +12,24 @@ class Category(models.Model):
         
 class Tag(models.Model):
     name        = models.CharField(max_length=50, default=None)
-    category_id = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category    = models.ForeignKey('Category', on_delete=models.CASCADE)
     
     def __str__(self):
         return '[{}] {}'.format(self.id, self.name)
     
     class Meta:
         db_table = 'tags'
+        
+class Board(models.Model):
+    writer    = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    title     = models.CharField(max_length=45)
+    content   = models.CharField(max_length=500)
+    password  = models.CharField(max_length=5000, default=None)
+    hits      = models.PositiveIntegerField(default=0)
+    tag       = models.ForeignKey('Tag', on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return '[{}] {}'.format(self.id, self.title)
+    
+    class Meta:
+        db_table = 'boards'
